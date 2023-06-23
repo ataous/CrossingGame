fb0a1dc3b1bfc02e7aa4ddd91ad82900419bdaec
Ata <attaataloo@gmail.com>
2023-06-23 19:02:48 +0330

    Create main.py

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import time
 from turtle import Screen
-from player import Player
+
 from car_manager import CarManager
+from player import Player
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+screen.onkey(player.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    if car_manager.check_touch_cars(player):
+        scoreboard.game_over()
+        break
+
+    if player.is_at_finish():
+        car_manager.level_up()
+        scoreboard.add_level()
+
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
